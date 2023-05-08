d2706e0a163d804bfd612a76396790ad8ded1dc1
junrdev <juniorprogrammer09@gmail.com>
2023-05-08 23:11:47 +0300

    modularized widgets

---
+++ muvi/app/src/main/java/com/star/muvi/screens/Widgets.kt
@@ -0,0 +1,19 @@
+package com.star.muvi.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import com.star.muvi.model.Movie
+
