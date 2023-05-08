69e4be6ffcca65dcd922659a710fe706b7ae57a0
junrdev <juniorprogrammer09@gmail.com>
2023-05-08 23:41:11 +0300

    added detailed view

--- muvi/app/src/main/java/com/star/muvi/screens/details/DetailsScreen.kt
+++ muvi/app/src/main/java/com/star/muvi/screens/details/DetailsScreen.kt
@@ -1,23 +1,33 @@
 package com.star.muvi.screens.details
 
 import android.annotation.SuppressLint
-import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.star.muvi.model.Movie
+import com.star.muvi.model.getMovies
+import com.star.muvi.widgets.MovieRow
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun DetailsScreen(navController: NavHostController, movie: String?) {
+fun DetailsScreen(navController: NavHostController, movieId: String?) {
 
+    //getting the current movie from the list
+    val currentMovie = getMovies().filter { it.id == movieId }
 
     Scaffold(
         topBar = {
@@ -33,21 +43,59 @@ fun DetailsScreen(navController: NavHostController, movie: String?) {
 
                     Spacer(modifier = Modifier.width(100.dp))
 
-                    Text(text = movie.toString(), fontWeight = FontWeight.Bold)
+                    Text(text = currentMovie.first().title, fontWeight = FontWeight.Bold)
                 }
             }
         },
-        modifier = Modifier.height(100.dp)
+        modifier = Modifier
     ) {
 
         Surface (modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()){
 
-            Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                Text(text = movie.toString(), style = MaterialTheme.typography.h5)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+
+//                Text(text = currentMovie.first().title)
+                MovieRow(it = currentMovie.first())
+
+                Spacer(modifier = Modifier.height(6.dp))
+                Divider()
+
+                Text(text = "Movie Images")
+
+                HorizontalMovieImagesDisplay(currentMovie)
+
             }
 
+        }
+    }
+}
+
+@Composable
+fun HorizontalMovieImagesDisplay(currentMovie: List<Movie>) {
+    LazyRow {
+        items(currentMovie.first().images) { img_url ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = 5.dp
+            ) {
+
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        ImageRequest.Builder(
+                            LocalContext.current
+                        ).data(data = img_url).apply(block = fun ImageRequest.Builder.() {
+                            crossfade(true)
+                        }).build()
+                    ), contentDescription = "Image"
+                )
+            }
         }
     }
 }
