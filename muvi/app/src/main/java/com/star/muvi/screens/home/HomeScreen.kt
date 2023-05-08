924353878e1fa77d4b68188e66991e859a87ad8e
junrdev <juniorprogrammer09@gmail.com>
2023-05-08 23:10:45 +0300

    changed movie passing

--- muvi/app/src/main/java/com/star/muvi/screens/home/HomeScreen.kt
+++ muvi/app/src/main/java/com/star/muvi/screens/home/HomeScreen.kt
@@ -1,23 +1,19 @@
 package com.star.muvi.screens.home
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.star.muvi.model.Movie
+import com.star.muvi.model.getMovies
 import com.star.muvi.navigation.MovieScreens
+import com.star.muvi.widgets.MovieRow
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -27,17 +23,13 @@ fun HomeScreen(navController: NavController) {
             Text(text = "Movies")
         }
     }) {
-        MainContent(navController= navController)
+        MainContent(navController= navController, movieList = getMovies())
     }
 }
 
 @Composable
 fun MainContent(
-    movieList: List<String> = listOf(
-    "Avatar",
-    "Piranha",
-    "Big Bang"
-),
+    movieList: List<Movie>,
 navController: NavController) {
 
 
@@ -56,39 +48,3 @@ navController: NavController) {
 }
 
 
-@Composable
-private fun MovieRow(it: String, onItemClick : (String) -> Unit ={}) {
-
-    Card(
-        modifier = Modifier
-            .padding(4.dp)
-            .fillMaxWidth()
-            .height(130.dp)
-            .clickable { onItemClick(it) },
-        shape = RoundedCornerShape(CornerSize(12.dp)),
-        elevation = 6.dp
-    ) {
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
-        ) {
-
-            //image
-            Surface(
-                modifier = Modifier
-                    .padding(12.dp)
-                    .size(100.dp),
-                shape = RectangleShape,
-                elevation = 4.dp
-            ) {
-
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "movie image")
-
-            }
-
-            //text
-            Text(text = it)
-        }
-    }
-}
