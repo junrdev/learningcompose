766d796cfd844af0a5c7cf84d081e70b07347eb6
junrdev <juniorprogrammer09@gmail.com>
2023-05-09 10:59:22 +0300

    added button colors

--- socialapp1/app/src/main/java/com/star/socialapp1/ui/theme/Color.kt
+++ socialapp1/app/src/main/java/com/star/socialapp1/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+//custom button colors
+val btn_primary = Color(0xFF2E7D32)
+val btn_secondary = Color(0xFF1565C0)
