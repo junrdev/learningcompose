a863b382bd8da71327329e60a26278eac078be47
junrdev <juniorprogrammer09@gmail.com>
2023-05-09 10:58:49 +0300

    added custom app bar

--- socialapp1/app/src/main/java/com/star/socialapp1/screens/HomeScreen.kt
+++ socialapp1/app/src/main/java/com/star/socialapp1/screens/HomeScreen.kt
@@ -1,10 +1,24 @@
 package com.star.socialapp1.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.star.socialapp1.model.User.Companion.getUsers
+import com.star.socialapp1.widgets.CustomTopBar
 
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(navController: NavController) {
+
+    Scaffold(modifier = Modifier.fillMaxWidth().fillMaxHeight(), topBar = {
+        CustomTopBar(modifier = Modifier.fillMaxWidth(), currentUser = getUsers().first(), navController = navController)
+    }) {
+
+    }
 
 }
